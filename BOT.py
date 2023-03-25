9bedb78f17c03b73837920622e2d38f37db78d94
unknown <0966459924qpwo@gmail.com>
2023-03-25 09:12:34 +0800

    v2.1

--- BOT.py
+++ BOT.py
@@ -7,10 +7,17 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 token__ = os.getenv("token")
+file_path = os.getenv("RTM-tools_config")
+
+discord_prefix = "$"
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="$", intents = intents)
+bot = commands.Bot(command_prefix=discord_prefix, intents = intents)
+
+help_mess = {
+    "arctan2": discord_prefix + "arctan2 [-r/-d] [-r/-d] <x> <y>",
+}
 
 def MissingRequiredArgument_process(message):
     data = message.split(" ")
@@ -24,6 +31,8 @@ async def on_ready():
 
 @bot.command()
 async def arctan2(ctx, *args):
+    error_state = False
+    error_message = []
     use = None
     i = int(0)
     arg = list(args)
@@ -53,24 +62,56 @@ async def arctan2(ctx, *args):
     
     ctx.a_F = 1
     ctx.b_F = 1
+    argu_error = []
     try:
         ctx.a_F = float(arg[0])
+    except:
+        argu_error.append("<x>")
+
+    try:
         ctx.b_F = float(arg[1])
     except:
-        await ctx.send(f"輸入參數錯誤")
+        argu_error.append("<y>")
+    if(len(arg) > 2):
+        error_message.append("多餘的參數")
+        error_state = True
+    
+    elif(len(arg) < 2):
+        err_miss = []
+        if(len(arg) == 0):
+            err_miss.append("<x>")
+        if(len(arg) <= 1):
+            err_miss.append("<y>")
+        error_message.append("缺少參數" + ", ".join(err_miss))
+        error_state = True
+    elif(len(argu_error) != 0):
+        error_message.append("輸入參數" + ",".join(argu_error) + "不是數字")
+        error_state = True
+    if(len(error_message) != 0):
+        await ctx.send(help_mess["arctan2"] + "\n" + "\n".join(error_message))
+    
     M = ctx.b_F/ctx.a_F
     rad = math.atan(M)
     deg = (rad * 180) / math.pi
     reply_str = ""
     if (use == "Degrees"):
-        reply_str = "degree: " + ('%.2f' % deg) + u'\N{DEGREE SIGN}'
+        reply_str = "degree: " + ('%.3f' % deg) + u'\N{DEGREE SIGN}'
     elif(use == "Radians"):
-        reply_str = "Radians: " + ('%.4f' % rad) + ' rad'
+        reply_str = "Radians: " + ('%.5f' % rad) + ' rad'
     else:
-        reply_str = "degree: " + ('%.2f' % deg) + u'\N{DEGREE SIGN}' + "     Radians: " + ('%.4f' % rad) + ' rad'
+        reply_str = "degree: " + ('%.3f' % deg) + u'\N{DEGREE SIGN}' + "     Radians: " + ('%.4f' % rad) + ' rad'
     print(use)
     print(arg)
-    await ctx.send(reply_str)
+    if(not(error_state)):
+        await ctx.send(reply_str)
+
+@bot.command()
+async def tan(ctx, degree):
+    radians = (90 - float(degree))*math.pi / 180
+    M = math.tan(radians)
+    await ctx.send("1.00, " + '%.2f'%M)
+    pass
+
 
 """
 @bot.event
